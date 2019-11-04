2e8a78e8b5ce8b555a755c0ad265c340bb996d2c
lsvosve <lsvosve@gmail.com>
2019-11-04 22:55:19 +0200

    my list products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
 private _albumUrl='../assets/album.json';
-private _productsUrl:'../assets/products.json';
+private _productsUrl='../assets/products.json';
   constructor(private _http: Http ) { }
   getAlbum(id:number):Observable<Album>{
 return this._http.get(this._albumUrl).map((response)=><Album>response.json());
