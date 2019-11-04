8c9761825279049cd1abda18478de50b7751eab5
lsvosve <lsvosve@gmail.com>
2019-11-04 23:13:19 +0200

    my Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
  const appRoutes:Routes=[
-   {path:'/products',component:ProductListComponent},
-   {path:'/product/:id',component:ProductPageComponent},
+   {path:'products',component:ProductListComponent},
+   {path:'product/:id',component:ProductPageComponent},
    {path:'',redirectTo:'products',pathMatch:'full'}
  ]
 @NgModule({
